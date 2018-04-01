047ea4b2f1324a95b1cb7277f240c1da88ecf5a2
Tomas <lightcalamar@gmail.com>
2018-04-01 08:24:51 +0200

    Add files via upload

---
+++ examples/STM32F/cobra_full_SD/Parametros.ino
@@ -0,0 +1,22 @@
+#if defined(__arm__)
+extern "C" char* sbrk(int incr);
+static int FreeStack() {
+  char top = 't';
+  return &top - reinterpret_cast<char*>(sbrk(0));
+}
+#endif
+
+char TXP[50];
+
+void Parametros()
+{
+  ram = FreeStack();
+  GD.SaveContext();
+  GD.ColorRGB(0x00ff00); 
+   sprintf(TXP,"F_CPU: %d MHz", (F_CPU/1000000));  GD.cmd_text(5, 0, 26, 0, TXP);
+   //sprintf(TXP,"F_RAM:  %d Kb", (ram/1024));  GD.cmd_text(5, 15, 26, 0, TXP);
+   //sprintf(TXP,"U_RAM:  %d Kb", ((262144-ram)/1024));  GD.cmd_text(5, 30, 26, 0, TXP);  //T36
+   //sprintf(TXP,"U_RAM:  %d Kb", ((131072-ram)/1024));  GD.cmd_text(5, 30, 26, 0, TXP);  //F429
+   
+  GD.RestoreContext();
+}
