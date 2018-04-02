b01668ddfcd05f090b055511d4955dae7fad745c
Tomas <lightcalamar@gmail.com>
2018-04-02 16:39:49 +0200

    Add files via upload

---
+++ examples/ESP8266/Playback_Video1/Playback_Video1.ino
@@ -0,0 +1,17 @@
+//#include <EEPROM.h>
+//#include <SPI.h>
+#include <GD23Z.h>
+
+void setup()
+{
+  EEPROM.begin(512);  //calibration on eeprom FOR esp8266
+  GD.begin();
+}
+
+void loop()
+{
+  MoviePlayer mp;
+
+  mp.begin("fun-1500.avi");
+  mp.play();
+}
